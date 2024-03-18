5423f0a15b079abb84174acc9a608c23b2835c3c
Sruthi-Maddineni <63460103+Sruthi556@users.noreply.github.com>
2024-03-18 20:11:11 +0530

    Add files via upload

---
+++ Leap_year.java
@@ -0,0 +1,18 @@
+public class Leap_year {
+    public static void main(String[] args) {
+
+
+        int n = 1999;
+        if(n%4==0)
+
+        {
+            System.out.println("leap year");
+        }
+    else
+
+        {
+            System.out.println("Not a Leap year");
+        }
+
+    }
+}
---
+++ Palindrome.java
@@ -0,0 +1,10 @@
+public class Palindrome {
+    public static void main(String[] args){
+        int n=1221;
+        
+
+
+
+        }
+    }
+
